e66eced6440ca072f00dc6cb9444d98d2c2e7612
JoelOccius <143296252+JoelOccius@users.noreply.github.com>
2025-02-23 10:21:04 -0500

    commit 7

---
+++ Exercice7-1/script.js
@@ -0,0 +1,57 @@
+// 
+// 1- Opérations mathématiques
+
+// Relevons un défit!Créer un programme qui exécute les étapes suivants:
+
+// Demandez un numéro avec un invite et enregistrez ce qui a été tapé dans une variable«firstNumber».
+// Demandez un deuxième numéro via invite et enregistrez ce qui a été saisi dans une variable«seconNumber».
+// Affichez le résulta de chacune des quatres opérations principales sur la console, au format «opération: résultat»
+// où opération c'est l'opération utiliser et le résultat est le résultat de l'opération.
+// Les opérations à effectuer sont:l'addition la soustrsction la multiplication et la division.
+
+// Observe bien:
+// Vous devez convertir les nombres saisis à l'invite au type de nombre,sinon les opération 
+// mathematiques ne fonctionneront pas  correctement. 
+// Example: let firstNumber = promt(entre premier numero:);
+// Laissez first convertir en number.Example Number(prompt9(enter first number)).
+
+
+// L'addition
+// let firstNumber = Number(prompt("entre le premier numero:"));
+// let convertieFirstNumber = Number(firstNumber)
+// let secondNumber = Number(prompt("entre le deuxièmenumer:"));
+
+// let add = firstNumber + secondNumber;
+
+
+// console.log(add);
+
+// La soustraction
+// let firstNumber = Number(prompt("entre le premier numero:"));
+// let convertieFirstNumber = Number(firstNumber)
+// let secondNumber = Number(prompt("entre le deuxièmenumer:"));
+
+// let sus = firstNumber - secondNumber;
+
+
+// console.log(sus);
+
+// La multiplication:
+// let firstNumber = Number(prompt("entre le premier numero:"));
+// let convertieFirstNumber = Number(firstNumber)
+// let secondNumber = Number(prompt("entre le deuxièmenumer:"));
+
+// let mul = firstNumber * secondNumber;
+
+
+// console.log(mul);
+
+// La division:
+// let firstNumber = Number(prompt("entre le premier numero:"));
+// let convertieFirstNumber = Number(firstNumber)
+// let secondNumber = Number(prompt("entre le deuxièmenumer:"));
+
+// let div = firstNumber / secondNumber;
+
+
+// console.log(div);
