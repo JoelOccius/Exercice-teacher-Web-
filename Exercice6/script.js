b9d2f7c80e651c6e41da4baa23335791e242f02d
JoelOccius <143296252+JoelOccius@users.noreply.github.com>
2025-02-23 10:14:22 -0500

    commit 6

---
+++ Exercice6/script.js
@@ -0,0 +1,9 @@
+// Créer un code qui imprime un nombre aléatoirement sur le console.
+// kod sa a jere nonb aleatwa soti nan 1 rive nan 100
+let nombreAleatoire = Math.floor(Math.random() * 100) + 1;
+console.log("Nombre aléatoire généré" , nombreAleatoire);
+// kod sa a afiche nonb nan console.
+
+
+
+
