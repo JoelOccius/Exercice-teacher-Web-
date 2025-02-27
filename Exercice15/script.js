23e9ebe3f6eb85a556f9f121b61e25ed428fd774
JoelOccius <143296252+JoelOccius@users.noreply.github.com>
2025-02-27 17:40:33 -0500

    fourth

---
+++ Exercice15/script.js
@@ -0,0 +1,17 @@
+// 15 Groupes - Travailler avec les commentaire.
+
+// Suivez les étapes c--dessous:
+
+// Modifier uniquementles valeurs initiales des variables x et y  dans le fichier script.js.
+// Le résultat devrait afficher  le numéro 20 dans l'alerte.
+
+// Remarque: n'ajoutez pas de nouvelle lignes de code.
+
+let x = 4;
+let y = 4;
+// Changement les valeurs de variables x et y.
+let z = x + y;
+
+let résultat = z * 2 + x;
+
+alert(résultat);//Doit afficher 20.
