359090e0efb5dc674cd85a10644d5ccd6e80fe12
JoelOccius <143296252+JoelOccius@users.noreply.github.com>
2025-02-27 17:29:04 -0500

    new commit

---
+++ Exercice12/script.js
@@ -0,0 +1,24 @@
+// 12 Groupe commentaire.
+// Dans le fichier script.js,répondez aux questions suivantes en utilisant 
+// les opérateurs de commentaire(// ou /* */):
+
+// Qu'est-ce que la programation?
+// Qu'est-ce que Javascript?
+// Quelle est la fonction de Javascript dans un navigateur web?
+// (Répondez en utilisant au moins trois lignes et les opérateurs /* et */).
+
+// Remarque: 
+// Les questions 1 et 2 doivent être répondues en une seule ligne avec//.
+// Les questions trois doit être repondue  en au moins trois lignes avec /* et */.
+
+
+// Qu'est-ce que la programation?Réponse:La programmation est le processus de création d'un ensemble d'instructions que l'ordinateur peut exécuter pour accomplir une tâche spécifique. Elle repose sur des langages de programmation qui permettent aux développeurs d’écrire du code compréhensible par les machines. La programmation est utilisée pour créer des logiciels, des applications web, des jeux vidéo, des systèmes embarqués, etc.
+
+// Qu'est-ce que JavaScript ?
+// JavaScript est un langage de programmation principalement utilisé pour rendre les pages web interactives. C’est un langage de script interprété qui s'exécute dans le navigateur de l'utilisateur. Il est souvent utilisé avec HTML (structure) et CSS (style) pour concevoir des sites web dynamiques et interactifs.
+
+// Quelle est la fonction de JavaScript dans un navigateur web ?
+
+/*Dans un navigateur web, JavaScript est utilisé pour :
+Rendre les pages interactives : Gestion des événements comme les clics, les mouvements de souris, ou l’entrée utilisateur.5 Manipuler le DOM (Document Object Model) : Modifier dynamiquement le contenu et la structure d'une page sans nécessiter un rechargement.Communiquer avec des serveurs : Envoyer et recevoir des données en arrière-plan grâce à AJAX ou Fetch API.
+Créer des animations et effets visuels : Développer des interfaces plus attrayantes et dynamiques.*/
